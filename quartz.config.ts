10243e61ea1a0512322bbfe3285e9f64e37e0f7e
thenameiskeith <85297507+thenameiskeith@users.noreply.github.com>
2024-07-23 20:17:55 -0500

    Quartz sync: Jul 23, 2024, 8:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "api", "Resources", "Archives", "99 - Meta"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
