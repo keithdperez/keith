2c53e765d801cca21f82abae1d38b8996e391a92
thenameiskeith <85297507+thenameiskeith@users.noreply.github.com>
2024-08-04 23:31:30 -0500

    Quartz sync: Aug 4, 2024, 11:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Keith Perez",
+    pageTitle: "keithderez",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
